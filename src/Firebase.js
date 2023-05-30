e19030673a4829c4e4c7972c5646598212418cdb
Waseem <bsf1801678@ue.edu.pk>
2023-05-30 13:37:17 +0500

    firebase

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCNElDVb7ohTWxJezW6U5ZTJV6tMz5lDNA",
+    authDomain: "shopping-app-1dd22.firebaseapp.com",
+    projectId: "shopping-app-1dd22",
+    storageBucket: "shopping-app-1dd22.appspot.com",
+    messagingSenderId: "778718297040",
+    appId: "1:778718297040:web:bc6e272d3f298e4a8c57d5"
+  };
+  const app = initializeApp(firebaseConfig);
+  export default app;  
